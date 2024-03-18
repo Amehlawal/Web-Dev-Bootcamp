3cb2cc3ebf8968ee6f9679fa2d6b6a8c9e93f5ee
Ameh Lawal <amehlawal@gmail.com>
2024-03-18 05:31:49 +0100

    improve the fizzbuzz, and create whosPaying

--- Intermediate-JavaScript/array.js
+++ Intermediate-JavaScript/array.js
@@ -1,41 +1,37 @@
-var output = [];
+var arr = [];
 var count = 1;
 
-
-
 function fizzBuzz() {
-    while (count <= 100) {
-        if (count % 3 === 0 && count % 5 === 0) {
-            output.push("FizzBuzz");
-        }
-        else if (count % 3 === 0) {
-            output.push("Fizz");
-        }
-        else if (count % 5 === 0) {
-            output.push("Buzz");
-        } else {
-            output.push(count);
-        }
-   
-        count++;
+  while (count <= 100) {
+    if (count % 3 === 0 && count % 5 === 0) {
+      arr.push("FizzBuzz");
+    } else if (count % 3 === 0) {
+      arr.push("Fizz");
+    } else if (count % 5 === 0) {
+      arr.push("Buzz");
+    } else {
+      arr.push(count);
     }
-    
-    console.log(output);
+
+    count++;
+  }
+
+  console.log(arr);
 }
 
 function beer() {
-    while (count <= 100) {
-        console.log(count + "bottles of beer on the wall");
-        count++;
-    }
+  while (count <= 100) {
+    console.log(count + "bottles of beer on the wall");
+    count++;
+  }
 }
 
 // exercise: given an array of names select a name from the names and print a statement
 
 function whosPaying(names) {
-    var numberOfPeoples = names.length;
-    var randomPersonPosition = Math.floor(Math.random() * numberOfPeoples);
-    var randomPerson = names[randomPersonPosition];
+  var numberOfPeoples = names.length;
+  var randomPersonPosition = Math.floor(Math.random() * numberOfPeoples);
+  var randomPerson = names[randomPersonPosition];
 
-    return randomPerson + " is going to buy lunch today";
+  return randomPerson + " is going to buy lunch today";
 }
