3185adbe14bc8c530c467f82d84a24287eb1e1e0
Ameh Lawal <amehlawal@gmail.com>
2024-03-19 05:24:19 +0100

    selecting html element using js

--- DOM/index.js
+++ DOM/index.js
@@ -2,3 +2,15 @@ var third = document.querySelector("ul").lastElementChild;
 third.innerHTML = 'holla'
 document.querySelector("h1").innerHTML = "<ul>Come</ul>";
 document.querySelector("h1").textContent = "<ul>Come</ul>";
+
+document.getElementsByTagName("li").length;
+document.getElementsByClassName("btn")[0].style.color = "red";
+
+document.querySelector(".btn")
+
+document.querySelector("li.item")
+document.querySelectorAll("li.item")
+
+// changing style using javascript
+
+document.querySelector(".btn").style.backgroundColor = "red";
