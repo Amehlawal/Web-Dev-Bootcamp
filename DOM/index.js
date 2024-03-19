61dc4d3506db482f1a4bee77e4eaec1bb5130a36
Ameh Lawal <amehlawal@gmail.com>
2024-03-19 04:58:27 +0100

    manipulate the dom using innerhtml and textcontent

---
+++ DOM/index.js
@@ -0,0 +1,4 @@
+var third = document.querySelector("ul").lastElementChild;
+third.innerHTML = 'holla'
+document.querySelector("h1").innerHTML = "<ul>Come</ul>";
+document.querySelector("h1").textContent = "<ul>Come</ul>";
